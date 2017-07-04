2c66ec085f9e102970cdfbb6425a1b0a01adea61
Pavel Emelyanov <xemul@virtuozzo.com>
2017-07-04 18:33:58 +0300

    net: Support "dumping" dummy sit device
    This device is an API endpoint, just skip it. Also, prepare the
    ground for sit dump.
    
    Issue #11
    
    Signed-off-by: Pavel Emelyanov <xemul@virtuozzo.com>

--- criu/net.c
+++ criu/net.c
@@ -658,6 +658,31 @@ static int dump_one_gre(struct ifinfomsg *ifi, char *kind,
 	return dump_unknown_device(ifi, kind, tb, ns, fds);
 }
 
+static int dump_one_sit(struct ifinfomsg *ifi, char *kind,
+		struct nlattr **tb, struct ns_id *ns, struct cr_imgset *fds)
+{
+	char *name;
+
+	if (strcmp(kind, "sit")) {
+		pr_err("SIT device with %s kind\n", kind);
+		return -1;
+	}
+
+	name = (char *)RTA_DATA(tb[IFLA_IFNAME]);
+	if (!name) {
+		pr_err("sit device %d has no name\n", ifi->ifi_index);
+		return -1;
+	}
+
+	if (!strcmp(name, "sit0")) {
+		pr_info("found %s, ignoring\n", name);
+		return 0;
+	}
+
+	pr_warn("SIT device %s not supported natively\n", name);
+	return dump_unknown_device(ifi, kind, tb, ns, fds);
+}
+
 static int list_one_link(struct nlmsghdr *hdr, struct ns_id *ns, void *arg)
 {
 	return 0;
@@ -700,6 +725,9 @@ static int dump_one_link(struct nlmsghdr *hdr, struct ns_id *ns, void *arg)
 		break;
 	case ARPHRD_IPGRE:
 		ret = dump_one_gre(ifi, kind, tb, ns, fds);
+		break;
+	case ARPHRD_SIT:
+		ret = dump_one_sit(ifi, kind, tb, ns, fds);
 		break;
 	default:
 unk:
